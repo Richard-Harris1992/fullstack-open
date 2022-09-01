02ce4d387c4caf606bb945d3fb8ecdb033070ece
Richard-Harris1992 <104505323+Richard-Harris1992@users.noreply.github.com>
2022-09-01 13:04:33 -0500

    I did the exercises, will still need to improve it

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,24 +1,43 @@
-const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a componenet";
-  const exercises3 = 14;
+/* Header takes care of rendering the name of the course, 
+  Content renders the parts and their number of exercises and
+   Total renders the total number of exercises. */
+  const Header = (props) => { 
+      return (
+        <div>
+          <p>"Half Stack application development"</p>
+        </div>
+      )
+  }
+  
+  const Content = () => {
+    const part1 = "Fundamentals of React";
+    const part2 = "Using props to pass data";
+    const part3 = "State of a componenet";
+    const exercises1 = 10;
+    const exercises2 = 7;
+    const exercises3 = 14;
+    
+    return (
+      <div>
+        <p>{part1} {exercises1}</p>
+        <p>{part2} {exercises2}</p>
+        <p>{part3} {exercises3}</p>
+      </div>
+    )
+  }
+
+  const Total = () => {
+    return(10 + 7 +14)
+  }
+
+
+   const App = () => {
 
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header />
+      <Content />
+      <Total />
     </div>
   )
 }
